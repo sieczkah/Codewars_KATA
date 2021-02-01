611c8ba42148d6ab85388d263b3fbfda31236a03
SieczkaH <huberts94@gmail.com>
2021-02-01 21:38:08 +0100

    Testing 1-2-3

--- 7 kyu/Testin 1-2-3.py
+++ 7 kyu/Testin 1-2-3.py
@@ -1,4 +1,5 @@
 """https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9"""
 
+
 def number(lines):
-    return [f'{n}: {l}' for n,l in enumerate(lines, 1)]
+    return [f'{n}: {l}' for n, l in enumerate(lines, 1)]
