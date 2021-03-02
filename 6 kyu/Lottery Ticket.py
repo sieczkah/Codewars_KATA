d03e732f8d59d98ac837fef45f70570f7e4166ad
SieczkaH <huberts94@gmail.com>
2021-03-02 22:54:56 +0100

    Added New Kata Day 23

---
+++ 6 kyu/Lottery Ticket.py
@@ -0,0 +1,5 @@
+"""https://www.codewars.com/kata/57f625992f4d53c24200070e/train/python"""
+
+def bingo(ticket,win):
+    mini_win = [ord_no for letters, ord_no in ticket if ord_no in [ord(i) for i in letters]]
+    return 'Winner!' if len(mini_win) >= win else "Loser!"
---
+++ 7 kyu/A Rule of Divisibility by 7.py
@@ -0,0 +1,7 @@
+"""https://www.codewars.com/kata/55e6f5e58f7817808e00002e/train/python"""
+
+def seven(m, steps=0):
+    while m > 99:
+        steps += 1
+        m = int(str(m)[:-1]) - int(str(m)[-1])*2 
+    return (m, steps)
