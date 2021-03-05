bc9be815c0fb059753a3485af630051ea125d74c
SieczkaH <huberts94@gmail.com>
2021-03-05 22:07:02 +0100

    Added New Kata Day 25

---
+++ 6 kyu/Sum consecutives.py
@@ -0,0 +1,13 @@
+"""https://www.codewars.com/kata/55eeddff3f64c954c2000059/train/python"""
+
+def sum_consecutives(s):
+    products = []
+    stack = [s[0]]
+    for i in s[1:]:
+        if stack and i == stack[-1]:
+            stack.append(i)
+        else:
+            products.append(sum(stack))
+            stack = [i]
+    products.append(sum(stack))
+    return products
---
+++ 7 kyu/Alphabet war.py
@@ -0,0 +1,12 @@
+"""https://www.codewars.com/kata/59377c53e66267c8f6000027/python"""
+
+l_side = {'w': 4, 'p': 3, 'b': 2, 's': 1}
+r_side = {'m': 4, 'q': 3, 'd': 2, 'z': 1}
+
+def alphabet_war(fight):
+    l_string = sum(l_side[i] for i in fight if i in 'wpbs')
+    r_string = sum(r_side[i] for i in fight if i in 'mqdz')
+    if l_string == r_string:
+        return "Let's fight again!"
+    else:
+        return "Right side wins!" if r_string > l_string else "Left side wins!"
