e71bad89642265e6f7abbd43362a2af157c6135a
SieczkaH <huberts94@gmail.com>
2021-03-06 20:20:28 +0100

    Added New Kata Day 25

---
+++ 6 kyu/Kebabize.py
@@ -0,0 +1,10 @@
+"""https://www.codewars.com/kata/57f8ff867a28db569e000c4a/train/python"""
+
+from string import ascii_lowercase as as_low
+from string import ascii_uppercase as as_up
+import re
+
+def kebabize(string):
+    string = re.sub(r'[^A-Za-z]', '', string)
+    words = [i if i in as_low else '-' + i.lower() for i in string]
+    return ''.join(words).strip('-')
---
+++ 6 kyu/Linked Lists - Length  Count.py
@@ -0,0 +1,24 @@
+"""https://www.codewars.com/kata/55beec7dd347078289000021/train/python"""
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+def length(node):
+    if node:
+        length = 1
+        while node.next:
+            length += 1
+            node = node.next
+        return length
+    else:
+        return 0
+    
+def count(node, data):
+    occur  = 0
+    while node is not None:
+        if node.data == data:
+            occur += 1
+        node = node.next
+    return occur
