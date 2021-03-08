46519e7096bb6d833298747fcb905c95625f9c53
SieczkaH <huberts94@gmail.com>
2021-03-08 22:15:19 +0100

    Added New Kata Day 27

---
+++ 6 kyu/Triangle type.py
@@ -0,0 +1,17 @@
+"""https://www.codewars.com/kata/53907ac3cd51b69f790006c5/train/python"""
+
+# Should return triangle type:
+#  0 : if triangle cannot be made with given sides
+#  1 : acute triangle
+#  2 : right triangle
+#  3 : obtuse triangle
+
+def triangle_type(a, b, c):
+    s1, s2, lng = sorted([a, b, c])
+    if lng >= s1 + s2:
+        return 0
+    cosa = (s1** 2 + s2 ** 2 - lng ** 2)/ (2 * s1 * s2)
+    if cosa == 0:
+        return 2
+    else:
+        return 1 if cosa > 0 else 3
