bb950ffc74bd9dcb6d3b72bae9d253d57e2dd32c
SieczkaH <huberts94@gmail.com>
2021-03-10 21:23:27 +0100

    Added New Kata Day 29

---
+++ 6 kyu/extract file name.py
@@ -0,0 +1,8 @@
+"""https://www.codewars.com/kata/597770e98b4b340e5b000071/train/python"""
+
+import re
+
+class FileNameExtractor:
+    def extract_file_name(dirty_file_name):
+        x = re.findall(r'\d+_(.+?\..+?)\.', dirty_file_name)
+        return x[0]
---
+++ 7 kyu/transform an array into a string.py
@@ -0,0 +1,4 @@
+"""https://www.codewars.com/kata/59a602dc57019008d900004e/train/python"""
+
+def transform(s):
+    return ''.join(map(str, s))
