947f586516583f656e2e2da59d0ae6ccc87011c8
Hubert Sieczka <huberts94@gmail.com>
2021-03-11 22:43:46 +0100

    Added New Kata Day 30

---
+++ 7 kyu/Sum of Cubes.py
@@ -0,0 +1,4 @@
+"""https://www.codewars.com/kata/59a8570b570190d313000037"""
+
+def sum_cubes(n):
+    return sum(i ** 3 for i in range(1, n + 1))
---
+++ 8 kyu/Transportation on vacation .py
@@ -0,0 +1,7 @@
+"""https://www.codewars.com/kata/568d0dd208ee69389d000016"""
+
+def rental_car_cost(d):
+    if d < 3:
+        return d * 40
+    else:
+        return d * 40 - 50 if d >= 7 else d * 40 - 20
