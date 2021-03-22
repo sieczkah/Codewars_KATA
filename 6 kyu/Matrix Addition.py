e760f5885c6955d4d919dee21d3d9007abc0f712
hsiec <huberts94@gmail.com>
2021-03-22 23:55:20 +0100

    Added New Kata Day 34

---
+++ 6 kyu/Matrix Addition.py
@@ -0,0 +1,5 @@
+"""https://www.codewars.com/kata/526233aefd4764272800036f"""
+
+def matrix_addition(a, b):
+    result = [[a[r][c] + b[r][c] for c in range(len(a))] for r in range(len(a))]
+    return result
---
+++ 6 kyu/The Vowel Code.py
@@ -0,0 +1,9 @@
+"""https://www.codewars.com/kata/53697be005f803751e0015aa"""
+
+c = {'a': '1', 'e': '2', 'i':'3', 'o': '4', 'u': '5'}
+dc = {'1': 'a', '2': 'e', '3':'i', '4': 'o', '5': 'u'}
+def encode(st):
+    return ''.join([c[i] if i in c.keys() else i for i in st])
+    
+def decode(st):
+    return ''.join([dc[i] if i in dc.keys() else i for i in st])
