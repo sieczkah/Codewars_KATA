d1d2340a2433c892d78572231d826c0cb8fb88b2
hsiec <huberts94@gmail.com>
2021-03-31 23:56:44 +0200

    Added New Kata Day 35

---
+++ 6 kyu/Checking Groups.py
@@ -0,0 +1,14 @@
+"""https://www.codewars.com/kata/54b80308488cb6cd31000161"""
+
+def group_check(s):
+    pairs = {'{': '}', '(': ')', '[': ']'}
+    p_stack = []
+    for par in s:
+        if p_stack and p_stack[-1] in pairs.keys():
+            if par == pairs[p_stack[-1]]:
+                p_stack.pop()
+            else:
+                p_stack.append(par)
+        else:
+            p_stack.append(par)
+    return len(p_stack) == 0
---
+++ 7 kyu/Find Fibonacci last digit.py
@@ -0,0 +1,7 @@
+"""https://www.codewars.com/kata/56b7251b81290caf76000978"""
+
+def get_last_digit(index):
+    fib = [0, 1]
+    for i in range(2, index + 1):
+        fib.append(fib[-2] + fib[-1])
+    return int(str(fib[-1])[-1])
