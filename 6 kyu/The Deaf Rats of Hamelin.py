fcc806dc73f97eeb4466176e4b95aac2c22b77e4
hsiec <huberts94@gmail.com>
2021-04-01 22:08:57 +0200

    Added New Kata Day 35

---
+++ 6 kyu/The Deaf Rats of Hamelin.py
@@ -0,0 +1,9 @@
+"""https://www.codewars.com/kata/598106cb34e205e074000031"""
+
+def count_deaf_rats(town):
+    t = town.replace(' ', '')
+    pip = t.find('P')
+    left = [t[i] + t[i+1] for i in range(0, pip, 2)]
+    right = [t[i] + t[i+1] for i in range(pip + 1, len(t), 2)]
+    return left.count('O~') + right.count('~O')
+
