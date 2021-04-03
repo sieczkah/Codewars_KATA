c2da323eb788845fbf3e8ea69646cc480c2ca371
hsiec <huberts94@gmail.com>
2021-04-03 23:14:25 +0200

    Added New Kata Day 36

---
+++ 5 kyu/The Hashtag Generator.py
@@ -0,0 +1,8 @@
+"""https://www.codewars.com/kata/52449b062fb80683ec000024"""
+
+def generate_hashtag(s):
+    if len(s.replace(' ','')) > 140 or s.replace(' ','') == '':
+        return False
+    else:
+        words = s.title().split()
+        return f"#{''.join(words)}"
---
+++ 6 kyu/Reverse every other word in the string.py
@@ -0,0 +1,6 @@
+"""https://www.codewars.com/kata/58d76854024c72c3e20000de"""
+
+def reverse_alternate(string):
+    words = string.split()
+    reversed = [words[i][::-1] if i % 2 else words[i] for i in range(len(words))]
+    return ' '.join(reversed)
