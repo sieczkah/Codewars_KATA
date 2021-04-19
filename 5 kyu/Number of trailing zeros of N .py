5bcae8bd5ec19c1c5b8016bb2c7dded97cd20859
hsiec <huberts94@gmail.com>
2021-04-19 19:13:02 +0200

    Added New Kata Day 38

---
+++ 5 kyu/Number of trailing zeros of N .py
@@ -0,0 +1,11 @@
+"""https://www.codewars.com/kata/52f787eb172a8b4ae1000a34"""
+
+def zeros(n):
+# Trailing zeroes are always produced by prime factors 2 and 5
+# as there is always more 2s then 5s we can assume that trailing 
+# zeroes are produced by 5
+    count = 0
+    while n >= 5:
+        n //= 5
+        count +=n
+    return count
---
+++ 6 kyu/Difference of 2.py
@@ -0,0 +1,6 @@
+"""https://www.codewars.com/kata/5340298112fa30e786000688"""
+
+def twos_difference(lst): 
+    x = set(lst)
+    return sorted([(i, i+2) for i in x if i+2 in x], key=sum)
+
