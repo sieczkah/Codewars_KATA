e0bdf3db123dc03230e2d9f8c3593a9f518ff05e
hsiec <huberts94@gmail.com>
2021-04-23 23:42:46 +0200

    Added New Kata Day 41

---
+++ 7 kyu/Alphabet symmetry.py
@@ -0,0 +1,10 @@
+"""https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0"""
+
+from string import ascii_lowercase as asc
+
+def ocupy(word):
+    return sum([1 if pos == asc.index(let) else 0 for pos, let in enumerate(word.lower())])
+
+def solve(arr):
+    return list(map(ocupy, arr))
+    
---
+++ 7 kyu/Maximum Length Difference.py
@@ -0,0 +1,12 @@
+"""https://www.codewars.com/kata/5663f5305102699bad000056"""
+
+def minmaxlen(arr):
+    len_arr = [len(i) for i in arr]
+    return min(len_arr), max(len_arr)
+
+def mxdiflg(a1, a2):
+    if a1 == [] or a2 == []:
+        return -1
+    min_a1, max_a1 = minmaxlen(a1)
+    min_a2, max_a2 = minmaxlen(a2)
+    return max([max_a2 - min_a1, max_a1 - min_a2])
