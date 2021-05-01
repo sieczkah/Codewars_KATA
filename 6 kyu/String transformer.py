85999d8843e1ad187b4e5a9ca91cebdfba6c3cb7
hsiec <huberts94@gmail.com>
2021-05-01 13:22:13 +0200

    Added New Kata Day 45

--- 6 kyu/String transformer.py
+++ 6 kyu/String transformer.py
@@ -1,4 +1,5 @@
-"""https://www.codewars.com/kata/5878520d52628a092f0002d0"""
+"""https://www.codewars.com/kata/5878520d52628a092f0002d0"""
+
 
 def string_transformer(s):
-    return ' '.join(list(map(lambda x: x.swapcase(), s.split(' ')[::-1])))
+    return ' '.join(s.swapcase().split(' ')[::-1])
