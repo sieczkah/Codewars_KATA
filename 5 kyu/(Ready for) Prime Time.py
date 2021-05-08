ed76b1839bec81c8e58a2e60fd592db0cfc9f16f
hsiec <huberts94@gmail.com>
2021-05-08 23:07:39 +0200

    Added New Kata Day 51

---
+++ 5 kyu/(Ready for) Prime Time.py
@@ -0,0 +1,15 @@
+"""https://www.codewars.com/kata/521ef596c106a935c0000519"""
+
+import math
+
+def is_prime(n):
+    if n in [0, 1]:
+        return False
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0 and n != i:
+            return False
+    return True
+    
+def prime(n):
+    primes = [x for x in range(n + 1) if is_prime(x)]
+    return primes
