f482d9e134e0d4c2e4b64affb30fa43c604b0af6
hsiec <huberts94@gmail.com>
2021-05-09 22:08:37 +0200

    Added New Kata Day 50

---
+++ 6 kyu/Sequences and Series.py
@@ -0,0 +1,9 @@
+"""https://www.codewars.com/kata/5254bd1357d59fbbe90001ec"""
+
+def get_score(n):
+    r = 50
+    x = 0
+    for i in range(n):
+        x += r
+        r += 50
+    return x
---
+++ 6 kyu/The Shell Game.py
@@ -0,0 +1,10 @@
+"""https://www.codewars.com/kata/546a3fea8a3502302a000cd2"""
+
+def find_the_ball(start, swaps):
+    ball = start
+    for x, y in swaps:
+        if x == ball:
+            ball = y
+        elif y == ball:
+            ball = x
+    return ball
