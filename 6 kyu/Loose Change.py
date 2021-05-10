f30a1601686d720227f3ca407c24efee978416c8
hsiec <huberts94@gmail.com>
2021-05-10 23:21:56 +0200

    Added New Kata Day 51

---
+++ 6 kyu/Loose Change.py
@@ -0,0 +1,11 @@
+"""https://www.codewars.com/kata/5571f712ddf00b54420000ee"""
+
+def loose_change(cents):
+    money = {'Quarters': 25, 'Dimes': 10, 'Nickels': 5, 'Pennies': 1}
+    if cents < 1:
+        return {coin: 0 for coin in money.keys()}
+    for coin, val in money.items():
+        money[coin] = cents // val
+        cents %= val
+    return  money
+        
