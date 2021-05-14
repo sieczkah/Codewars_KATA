073888c9c746bece29d33fe30ed98d24923ff7b8
hsiec <huberts94@gmail.com>
2021-05-14 21:33:50 +0200

    Added New Kata Day 53

---
+++ 6 kyu/Pascal s Triangle  2.py
@@ -0,0 +1,14 @@
+"""https://www.codewars.com/kata/52945ce49bb38560fe0001d9"""
+
+def pascal(p):
+    tri = []
+    for j in range(p):
+        line = []
+        for k in range(j + 1):
+            if k == 0 or k == j:
+                line.append(1)
+            else:
+                line.append(tri[j-1][k-1] + tri[j-1][k])
+        tri.append(line)
+    return tri
+                
