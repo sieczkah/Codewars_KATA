3a514d04524fd6c4c14a570515fd94dd4911e230
hsiec <huberts94@gmail.com>
2021-05-19 14:45:26 +0200

    Added New Kata Day 54

---
+++ 6 kyu/Error correction  1 - Hamming Code.py
@@ -0,0 +1,15 @@
+"""https://www.codewars.com/kata/5ef9ca8b76be6d001d5e1c3e"""
+
+def flip_filter(grp):
+    return '0' if grp.count('0') > grp.count('1') else '1'
+
+def encode(string):
+    bit_str = ''.join(bin(ord(i))[2:].zfill(8) for i in string)
+    return ''.join(list(map(lambda x : x*3, bit_str)))
+
+def decode(bits):
+    bit_str = ''.join([flip_filter(bits[i:i+3]) for i in range(0, len(bits), 3)])
+    msg = ''
+    for i in range(0, len(bit_str), 8):
+        msg += chr((int(bit_str[i:i+8], 2)))
+    return msg
---
+++ 7 kyu/Speed Control.py
@@ -0,0 +1,6 @@
+"""https://www.codewars.com/kata/56484848ba95170a8000004d"""
+
+def gps(s, x):
+    speed = [(x[i + 1] - x[i]) / s * 3600 for i in range(len(x)-1)]
+    return max(speed) if speed else 0
+
